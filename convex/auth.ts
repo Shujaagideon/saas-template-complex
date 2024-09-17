231128874a86b595fc41b61bb29ed2584866c9bd
Shujaa Gideon <shujaagideon@gmail.com>
2024-09-18 02:55:41 +0300

    google login

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,7 @@
 import { convexAuth } from "@convex-dev/auth/server";
 import { ResendOTP } from "./OTP/ResendOTP";
+import Google from '@auth/core/providers/google'
 
 export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [ResendOTP],
+  providers: [ResendOTP, Google],
 });
--- src/app/sign_in/SignInWithOAuth.tsx
+++ src/app/sign_in/SignInWithOAuth.tsx
@@ -4,7 +4,7 @@ import { SignInWithGoogle } from "./SignInWithGoogle";
 export function SignInWithOAuth() {
   return (
     <div className="flex flex-col min-[460px]:flex-row w-full gap-2 items-stretch">
-      <SignInWithGitHub />
+      {/* <SignInWithGitHub /> */}
       <SignInWithGoogle />
     </div>
   );
